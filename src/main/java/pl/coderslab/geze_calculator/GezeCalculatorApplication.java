b97c2b965f05c650be1711f8453936287d4bb9d0
Szymon Jaroszyński <58995784+szymper206@users.noreply.github.com>
2020-09-06 12:52:19 +0200

    init commit

---
+++ src/main/java/pl/coderslab/geze_calculator/GezeCalculatorApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.geze_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GezeCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GezeCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/coderslab/geze_calculator/GezeCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.geze_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GezeCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
